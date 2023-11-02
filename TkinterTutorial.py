a917cd5f44f78382410f1b97096e10cc527fd8d1
Cameron Beebe <118768500+fencingbuddha@users.noreply.github.com>
2023-11-02 15:45:07 -0400

    Add files via upload
    I have commented out the previous label and button text and added a label with an entry box that is prefilled with the text Python.

--- TkinterTutorial.py
+++ TkinterTutorial.py
@@ -1,28 +1,30 @@
 import tkinter as tk
 
 window = tk.Tk()
-greeting = tk.Label(
-    text="Hello, Tkinter",
-    foreground="white", # Set the text color to white
-    bg="#34A2FE", #set the background color to light blue while using the abbreviated for backgroud bg. 
-    width=10,
-    height=10
-)
-greeting.pack()
+#greeting = tk.Label(
+#    text="Hello, Tkinter",
+#    foreground="white", # Set the text color to white
+#    bg="#34A2FE", #set the background color to light blue while using the abbreviated for backgroud bg. 
+#    width=10,
+#    height=10
+#)
+#greeting.pack()
 
-button = tk.Button(
-    text = "Click Me!",
-    width=25,
-    height=5,
-    bg="blue",
-    fg="yellow"
-)
-button.pack()
+#button = tk.Button(
+#    text = "Click Me!",
+#    width=25,
+#    height=5,
+#    bg="blue",
+#    fg="yellow"
+#)
+#button.pack()
 
 label = tk.Label(text='Name')
 entry = tk.Entry()
 label.pack()
 entry.pack()
+
+name = entry.insert(0, "Python")
 
 window.mainloop()
 
